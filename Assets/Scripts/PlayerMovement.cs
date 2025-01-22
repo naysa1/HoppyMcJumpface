bbb4ceabe39e4dbc445b66b9a8ce50acb45810ab
Naysa Chopra <naysachopra@gmail.com>
2025-01-21 22:55:10 -0800

    added running + jump animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,15 @@ public class PlayerMovement : MonoBehaviour
    [SerializeField]private float speed;
    [SerializeField]private float jumpspeed;
    private Vector3 originalScale;
-   private Animator 
+   private Animator anim;
+   private bool Grounded;
 
    private void Awake() // this method will be called everytime the script loads
    {
+        // grab references for rigidbody & animator from object
         body = GetComponent<Rigidbody2D>(); // a way to access rigidbody2D
         originalScale = transform.localScale;
+        anim = GetComponent<Animator>();
    }
 
    private void Update()
@@ -29,9 +32,25 @@ public class PlayerMovement : MonoBehaviour
                 transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z);
             }
 
-        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
+        if (Grounded && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)))
             {
                 body.linearVelocity = new Vector2(body.linearVelocity.x, jumpspeed);
             }
+        
+        // set animator parameters
+        anim.SetBool("Run", horizontalInput != 0); // if input = 0 and if 0 != 0 then thats FALSE so, no input means player not running
+        anim.SetBool("Grounded", Grounded);
    }
+
+
+   private void Jump() {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        Grounded = false;
+   }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Ground") {
+            Grounded = true;
+        }
+    }
 }
