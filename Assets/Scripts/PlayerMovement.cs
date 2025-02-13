19cb2fb3f3db3c5dd277020978aedcac9bcf4904
mcbride03 <michaelmcb2003@gmail.com>
2025-02-13 13:09:55 -0800

    bounce
    player is bounced off walls now

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
+using Unity.VisualScripting;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.WSA;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,12 +10,22 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float maxJumpValue = 20f; // Maximum jump force
     [SerializeField] private float jumpChargeRate = 40f; // Rate at which jump force increases
     private float jumpCharge = 5f; // Current jump charge
+    private float slideFactor = 5f;
     private Vector3 originalScale;
     private Animator anim;
     [SerializeField] private bool Grounded;
 
     private bool isChargingJump = false;
     private bool movementLocked = false;
+    private bool isSliding = false;
+    private bool ignoreInput;
+    private float bounceDir = 0f;
+    private const float wallCheckRadius = 0.5f;
+
+    public Transform wallCheckCollider;
+    public LayerMask wallLayer;
+
+    
 
     private void Awake()
     {
@@ -21,18 +33,35 @@ public class PlayerMovement : MonoBehaviour
         originalScale = transform.localScale;
         anim = GetComponent<Animator>();
         body.freezeRotation = true;
+        ignoreInput = false;
     }
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal"); // saving user input 
-        if (movementLocked == false)
+        // movemvent script
+        float horizontalInput;
+        if (!ignoreInput)
+        {
+            horizontalInput = Input.GetAxis("Horizontal"); // saving user input
+            if (horizontalInput < 0f)
+            {
+                bounceDir = 1.0f;
+            } else if (horizontalInput > 0f)
+            {
+                bounceDir = -1.0f;
+            }
+        } else
+        {
+            horizontalInput = 1.0f;
+        }
+        if (!movementLocked && !ignoreInput)
         {
             body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y); // moving user horizontally 
-        } else
+        } else if (movementLocked)
         {
             body.linearVelocity = Vector2.zero; // lock movement 
         }
+
         // Flip sprite based on movement direction
         if (horizontalInput > 0.01f)
         {
@@ -69,9 +98,47 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Tilemap"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
             Grounded = true;
+            ignoreInput = false;
+            movementLocked = false;
+        }
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            ignoreInput = true;
+            Grounded = false;
+
+            body.linearVelocity = new Vector2(bounceDir*(speed/2), body.linearVelocity.y); // moving user horizontally
+            
+            //WallCheck();
+        }
+    }
+
+    void WallCheck()
+    {
+        //if touching a wall
+        //moving towards wall
+        //falling
+        //not grounded
+        //slide on wall
+        if(Physics2D.OverlapCircle(wallCheckCollider.position, wallCheckRadius, wallLayer)
+            && Mathf.Abs(Input.GetAxis("Horizontal")) > 1
+            && body.linearVelocityY <= 0
+            && !Grounded)
+        {
+            Vector2 v = body.linearVelocity;
+            v.y = -slideFactor;
+            body.linearVelocity = v;
+            isSliding = true;
+            if (!isSliding)
+            {
+                //Grounded = movementLocked = false;
+            }
+        }
+        else
+        {
+            isSliding = false;
         }
     }
 }
