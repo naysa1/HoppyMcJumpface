96c0f4e461f02decc5a9b6223878ecf530ab726d
Naysa Chopra <naysachopra@gmail.com>
2025-05-14 20:23:32 -0700

    fixed layers again
    meow

--- Assets/Scripts/MenuParralax.cs
+++ Assets/Scripts/MenuParralax.cs
@@ -1,22 +1,19 @@
 using UnityEngine;
-
-public class MenuParralax : MonoBehaviour
+ 
+public class MenuParallax : MonoBehaviour
 {
     public float offsetMultiplier = 1f;
     public float smoothTime = .3f;
+ 
     private Vector2 startPosition;
     private Vector3 velocity;
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+ 
+    private void Start()
     {
         startPosition = transform.position;
-        
     }
-
-    // Update is called once per frame
-    void Update()
+ 
+    private void Update()
     {
         Vector2 offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         transform.position = Vector3.SmoothDamp(transform.position, startPosition + (offset * offsetMultiplier), ref velocity, smoothTime);
